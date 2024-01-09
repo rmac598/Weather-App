a9236b978bbd79559f286178db51bef4bced368d
Ryan McIntyre <rmcintyre598@gmail.com>
2024-01-09 09:37:50 -0500

    added humidity, wind speed, temp

--- scripts.js
+++ scripts.js
@@ -29,8 +29,16 @@ const getfiveday = async(lat,lon)=>{
     // get the body out of the response
    const forecast = await response.json(); 
    console.log(forecast);
+
+   var start = dayjs().add(1, "day").startof("day").unix();
+   var end = dayjs().add(6, "day").startof("day").unix();
    ///log the data
+  
    $(".Forecast").append($(`<div>${forecast.list[0].dt_txt}</div>`));
+   $(".Forecast").append($(`<div>${forecast.list[0].main.temp}</div>`));
+   $(".Forecast").append($(`<div>${forecast.list[0].wind.speed}</div>`));
+   $(".Forecast").append($(`<div>${forecast.list[0].main.humidity}</div>`));
+   
  //  const Image = $(`<img>`);
   // Image.attr('src',`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
 //$(".Forecast").append(Image);
@@ -41,10 +49,11 @@ const getfiveday = async(lat,lon)=>{
   // console.log(weather.name);
  //  console.log(weather.main.temp);
   // console.log(weather.wind.speed);
-   
+ 
+   }
  
 
-}
+//}
      
 
 const getCoords=  async(city) =>{
